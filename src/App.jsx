6915aed32d97c9e8c9c973f70f92cfceb13e6996
Jun Ahn <junahn45@gmail.com>
2022-06-20 19:35:17 -0700

    cleaned up code & added comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ function App() {
   const [user, setUser] = useState('');
   const [allUser, setAllUser] = useState('');
 
+  // updates user based on the text input
   function updateUser(event) {
     setUser(event.target.value);
   }
 
+  // updates the user in the database existing in the backend
+  // when calling to the backend, we use Axios, promise-based HTTP client.
+  // the syntax goes axios.<type of request>('<http protocal with function desired to call>')
   function updateUserDb(event) {
     axios.post('http://localhost:8080/addUser', { name: user });
   }
 
+  // deleted the user in the database
   function deleteUserDb(event) {
     const newUrl = `http://localhost:8080/deleteUser/${user}`;
     axios.delete(newUrl);
   }
 
   // HTTP protocal, this line of code is basically like Postman,
-  // calling to the pp.get('/getUser') function in the backend
+  // calling to the get('/getUser') function in the backend
   useEffect(() => {
     axios.get('http://localhost:8080/getUser').then((dbUser) => {
       const userList = dbUser.data;
